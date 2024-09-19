552afaf212d7ca5d8a9934866425c24bf89d14d2
Nihal Bhovare <44982529+nbhovare@users.noreply.github.com>
2024-09-19 16:09:38 +0530

    Install tree as pre-req

--- linux/pre.sh
+++ linux/pre.sh
@@ -5,5 +5,6 @@
 sudo apt install openjdk-17-jre -y
 sudo apt install fontconfig -y
 sudo apt install jq -y
+sudo apt install tree -y
 sudo apt install docker.io -y
 # sudo apt install ubuntu-desktop -y
